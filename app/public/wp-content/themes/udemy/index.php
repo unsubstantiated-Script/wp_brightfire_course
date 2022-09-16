58acf7a4a5cb7bba7a8dccec6c22e3a3a2370503
Justin Fulton <justin@brightfire.com>
2022-09-16 14:17:08 -0700

    initials

---
+++ app/public/wp-content/themes/udemy/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>;>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php wp_head(); ?>
+    <title>Document</title>
+</head>
+<body <?php body_class('example'); ?>>
+<?php wp_body_open(); ?>
+<h1>Heya!</h1>
+<?php wp_footer(); ?>
+</body>
+</html>
