f5cc84e73a848411e0615b6a29811b115317cfe1
Justin Fulton <justin@brightfire.com>
2022-09-20 09:00:05 -0700

    initial plugin setup

---
+++ app/public/wp-content/plugins/udemy-plus/index.php
@@ -0,0 +1,21 @@
+<?php //Kak is lacking!
+
+/**
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        https://udemy.com
+ * Description:       A plugin for adding blocks to a theme.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.4
+ * Author:            Justin Fulton
+ * Author URI:        https://unsubstantiatedscript.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
+
+if(!function_exists('add_action')){
+    echo 'Seems as if you do not belong here...';
+    exit;
+}
