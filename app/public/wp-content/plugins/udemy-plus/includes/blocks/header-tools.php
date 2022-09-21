e53f7eff057562ed8ce25c1eaa46ee026c9a09da
Justin Fulton <justin@brightfire.com>
2022-09-21 15:00:26 -0700

    Updating login toggle

--- app/public/wp-content/plugins/udemy-plus/includes/blocks/header-tools.php
+++ app/public/wp-content/plugins/udemy-plus/includes/blocks/header-tools.php
@@ -1,5 +1,39 @@
 <?php
 
-function up_header_tools_render_cb() {
+function up_header_tools_render_cb($atts)
+{
+    $heading = esc_html($atts['content']);
 
+    if ($atts['showCategory']) {
+        $heading = get_the_archive_title();
+    }
+
+    ob_start();
+    ?>
+    <div class="wp-block-udemy-plus-header-tools">
+
+        <?php
+        if ($atts['showAuth']) {
+            ?>
+            <a class="signin-link open-modal" href="#">
+                <div class="signin-icon">
+                    <i class="bi bi-person-circle"></i>
+                </div>
+                <div class="signin-text">
+                    <small>Hello, Sign in</small>
+                    My Account
+                </div>
+            </a>
+            <?php
+        }
+        ?>
+    </div>
+
+
+    <?php
+
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
 }
