0a8f32f08686a7d62d0aa0aa4886edecb1550705
Justin Fulton <justin@brightfire.com>
2022-09-21 12:58:36 -0700

    Setup for custom header is complete

--- app/public/wp-content/plugins/udemy-plus/src/blocks/page-header/index.js
+++ app/public/wp-content/plugins/udemy-plus/src/blocks/page-header/index.js
@@ -1,29 +1,45 @@
 import { registerBlockType } from '@wordpress/blocks';
 import {
-    useBlockProps, RichText
+    useBlockProps, RichText, InspectorControls
 } from '@wordpress/block-editor';
-import icons from '../../icons.js'
+import { PanelBody, ToggleControl } from '@wordpress/components'
 import { __ } from '@wordpress/i18n'
+import icons from '../../icons.js'
 import './main.css'
 
 registerBlockType('udemy-plus/page-header', {
-    icon: icons.primary,
-    edit({ attributes, setAttributes }) {
+    icon: icons.primary, edit({ attributes, setAttributes }) {
         const blockProps = useBlockProps();
-        const { content } = attributes
-        return (
-            <>
-                <div { ...blockProps }>
-                    <div className="inner-page-header">
-                        <RichText
-                            tagName="h1"
-                            placeholder={ __("Heading", "udemy-plus") }
-                            value={ content }
-                            onChange={ content => setAttributes({ content }) }
-                        />
-                    </div>
+        const { content, showCategory } = attributes
+        return ( <>
+            <InspectorControls>
+                <PanelBody title={ __('General', 'udemy-plus') }>
+                    <ToggleControl
+                        label={ __('Show Category', 'udemy-plus') }
+                        checked={ showCategory }
+                        onChange={ showCategory => setAttributes({ showCategory }) }
+                        help={
+                            showCategory ? __('Category Shown', 'udemy=plus') : __('Custom Content Shown', 'udemy=plus')
+                        }
+                    />
+                </PanelBody>
+            </InspectorControls>
+            <div { ...blockProps }>
+                <div className="inner-page-header">
+                    {
+                        showCategory ?
+                            <h1>{ __('Category: Some Category', 'udemy-plus') }</h1> :
+                            <RichText
+                                tagName="h1"
+                                placeholder={ __("Heading", "udemy-plus") }
+                                value={ content }
+                                onChange={ content => setAttributes({ content }) }
+                            />
+                    }
+
                 </div>
-            </>
-        );
+            </div>
+        </> );
     }
 });
+
