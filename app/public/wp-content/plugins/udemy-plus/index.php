85fb771a7496db8351d230a2bc0013c690e4a53a
Justin Fulton <justin@brightfire.com>
2022-09-22 14:40:46 -0700

    Updating admin with new post info

--- app/public/wp-content/plugins/udemy-plus/index.php
+++ app/public/wp-content/plugins/udemy-plus/index.php
@@ -44,3 +44,4 @@ foreach ($allFiles as $filename) {
 add_action('init', 'up_register_blocks');
 add_action('rest_api_init', 'up_rest_api_init');
 add_action('wp_enqueue_scripts', 'up_enqueue_scripts');
+add_action('init', 'up_recipe_post_type');
