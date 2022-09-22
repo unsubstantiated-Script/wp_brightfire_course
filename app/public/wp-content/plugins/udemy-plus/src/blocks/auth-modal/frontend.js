e8ee37e90aa99f3208e58070752b8f98e77f5fae
Justin Fulton <justin@brightfire.com>
2022-09-22 10:24:14 -0700

    Sign up form complete

--- app/public/wp-content/plugins/udemy-plus/src/blocks/auth-modal/frontend.js
+++ app/public/wp-content/plugins/udemy-plus/src/blocks/auth-modal/frontend.js
@@ -22,4 +22,28 @@ document.addEventListener('DOMContentLoaded', () => {
             modalEl.classList.remove('modal-show')
         })
     })
+
+    const tabs = document.querySelectorAll('.tabs a')
+    const signinForm = document.querySelector('#signin-tab')
+    const signupForm = document.querySelector('#signup-tab')
+
+    tabs.forEach((tab) => {
+        tab.addEventListener('click', e => {
+            e.preventDefault()
+            tabs.forEach(currentTab => {
+                currentTab.classList.remove('active-tab')
+            })
+            e.currentTarget.classList.add('active-tab')
+
+            const activeTab = e.currentTarget.getAttribute('href')
+
+            if (activeTab === '#signin-tab') {
+                signinForm.style.display = 'block'
+                signupForm.style.display = 'none'
+            } else {
+                signinForm.style.display = 'none'
+                signupForm.style.display = 'block'
+            }
+        })
+    })
 })
