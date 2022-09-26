68112d2ef337792b4bdcacb53820724154418ca7
Justin Fulton <justin@brightfire.com>
2022-09-26 14:55:15 -0700

    added ability to remove plugin entirely

---
+++ app/public/wp-content/plugins/udemy-plus/uninstall.php
@@ -0,0 +1,12 @@
+<?php
+
+if(!defined("WP_UNINSTALL_PLUGIN")) {
+    exit;
+}
+
+delete_option("up_options");
+
+global $wpdb;
+$wpdb->query(
+    "DROP TABLE IF EXISTS {$wpdb->prefix}recipe_ratings"
+);
