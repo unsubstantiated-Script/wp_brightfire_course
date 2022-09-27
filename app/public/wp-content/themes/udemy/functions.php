6c60f9551ce9e23ef4927d548b2755a09a3cfa89
Justin Fulton <justin@brightfire.com>
2022-09-27 06:29:24 -0700

    course complete with final package bundling classes

--- app/public/wp-content/themes/udemy/functions.php
+++ app/public/wp-content/themes/udemy/functions.php
@@ -7,9 +7,12 @@
 include(get_theme_file_path('/includes/front/enqueue.php'));
 include(get_theme_file_path('/includes/front/head.php'));
 include(get_theme_file_path('/includes/setup.php'));
+include(get_theme_file_path('/includes/class-tgm-plugin-activation.php'));
+include(get_theme_file_path('/includes/register-plugins.php'));
 
 
 //Hooks
 add_action('wp_enqueue_scripts', 'u_enqueue');
 //Lower number moves it up in the queue
 add_action('after_setup_theme', 'u_setup_theme');
+add_action('tgmpa_register', 'u_register_plugins');
