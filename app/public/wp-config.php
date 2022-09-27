067fbe2e244b9e842a8c375930919fc19213bf6b
Justin Fulton <justin@brightfire.com>
2022-09-27 06:05:30 -0700

    updating project to tidy up

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -75,6 +75,7 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', true );
 define( 'WP_DISABLE_FATAL_ERROR_HANDLER', true );
+define( 'SAVEQUERIES', true );
 
 
 /* Add any custom values between this line and the "stop editing" line. */
