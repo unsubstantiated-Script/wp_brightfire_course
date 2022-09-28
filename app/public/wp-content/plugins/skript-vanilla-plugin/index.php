0bc75237980090e6ce0086547c8eb6e679bab765
Justin Fulton <justin@brightfire.com>
2022-09-28 10:57:59 -0700

    Working on Vanilla WP Plugin

---
+++ app/public/wp-content/plugins/skript-vanilla-plugin/index.php
@@ -0,0 +1,1 @@
+<?php //sugar honey honey
---
+++ app/public/wp-content/plugins/skript-vanilla-plugin/skript-vanilla-plugin.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Plugin Name: Skript Vanilla
+ * Plugin URI: https://www.unsubstantiatedscript.com/
+ * Description: A vanilla WP Plugin to test things out
+ * Version: 0.1
+ * Author: Skript
+ * Author URI: https://www.unsubstantiatedscript.com/
+ **/
+
+defined('ABSPATH') or die('I\'m sorry Dave, I can\'t do that\!');
+
+
+class VanillaPlugin
+{
+
+//This is really useful when you want to update or wipe the DB of the installation...
+    function __construct()
+    {
+        // OOP way of instantiating the add_action...
+        add_action('init', [$this, 'custom_post_type']);
+    }
+
+ function activate()
+    {
+        //Generate a Custom Post Type
+        //Flush Rewrite Rules
+    }
+
+    function deactivate()
+    {
+        //Flush Rewrite Rules
+    }
+
+    function uninstall()
+    {
+        // Delete Custom Post Type
+        // Delete all the plugin data from the DB
+    }
+
+    function custom_post_type()
+    {
+        register_post_type('book', ['public' => 'true']);
+    }
+
+
+}
+
+if (class_exists('VanillaPlugin')) {
+    $vanillaPlugin = new VanillaPlugin('We are good to go Red Leader');
+}
+
+// activation
+register_activation_hook(__FILE__, [$vanillaPlugin, 'activate']);
+
+
+// deactivation
+register_deactivation_hook(__FILE__, [$vanillaPlugin, 'deactivate']);
+
+// uninstall
